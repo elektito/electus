fc61714aab8d9d3d670fcec8a57da8b17e33de31
Mostafa Razavi <mostafa@sepent.com>
2018-12-19 21:14:31 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Electus: Linux Really from Scratch
+
+See [this post][1] in my blog.
+
+[1]: http://elektito.com/2018/12/15/linux-really-from-scratch/
---
+++ pkgs/hello/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+  printf("Hello, World!\n");
+  printf("This is your friendly init system.\n");
+  printf("Just hanging here...\n");
+  for (;;);
+  return 0;
+}
