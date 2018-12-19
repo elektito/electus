61164941e75f0fbac333c6435d48c43f679e6608
Mostafa Razavi <mostafa@sepent.com>
2018-12-19 21:45:59 +0100

    A bit more proper init.
    Init now reaps zombies and launches a shell.

--- pkgs/hello/hello.c
+++ pkgs/hello/hello.c
@@ -1,11 +1,70 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
 #include <stdio.h>
+
+static void
+handle_sigchld(int sig) {
+  int saved_errno = errno;
+
+  /* reap orphaned children. rip. */
+  while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) {}
+
+  errno = saved_errno;
+}
+
+static int
+run_program(const char *path)
+{
+  pid_t child;
+  int ret;
+  siginfo_t info;
+
+  child = fork();
+  if (child) {
+    waitid(P_PID, child, &info, WEXITED);
+    return info.si_status;
+  } else {
+    execl(path, path, NULL);
+    printf("Could not run: %s\n", path);
+    printf("    %s\n", strerror(errno));
+    exit(255);
+  }
+}
+
+static void
+launch_login(void)
+{
+  if (!fork()) {
+    execl("/bin/bash", "/bin/bash", NULL);
+  }
+}
 
 int
 main(int argc, char *argv[])
 {
-  printf("Hello, World!\n");
-  printf("This is your friendly init system.\n");
-  printf("Just hanging here...\n");
-  for (;;);
+  struct sigaction sa;
+
+  /* register sigchld handler */
+  sa.sa_handler = &handle_sigchld;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+  if (sigaction(SIGCHLD, &sa, 0) == -1) {
+    perror(0);
+    return 1;
+  }
+
+  run_program("/etc/rc.local");
+
+  launch_login();
+
+  for (;;) {
+    usleep(600 * 1000000);
+  }
+
   return 0;
 }
